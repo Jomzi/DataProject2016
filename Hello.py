f4d5d668182f570fcab7a6fa5d4278d2a8d85d7e
Jomzi <jameso.hanlon1@gmail.com>
2016-11-30 21:41:00 +0000

    Add files via upload

--- Hello.py
+++ Hello.py
@@ -11,12 +11,17 @@ app.config['MYSQL_DATABASE_PASSWORD'] = 'krock47'
 app.config['MYSQL_DATABASE_DB'] = 'zoo'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
+
+# Connect to the database
 conn = mysql.connect()
 cursor = conn.cursor()
+
+# Query the Animals table
 cursor.execute("SELECT * FROM ANIMALS")
 animals = cursor.fetchall()
 conn.close()
 
+# View function mapped to the root url of the site
 @app.route('/')
 def hello_world():
   return render_template('index.html', animals = animals)
